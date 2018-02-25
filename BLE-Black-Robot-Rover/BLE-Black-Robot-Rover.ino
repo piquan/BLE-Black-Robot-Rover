b55da9b25dbb51d0e784524cfb704a211f032b9e
Joel Ray Holveck <joelh@piquan.org>
2018-02-24 17:23:24 -0800

    Reorder
    Rearrange everything to make it easier to maintain.
    
    The code was pretty scattered around.  This commit rearranges it.  The
    only thing this commit does is to move existing code around
    vertically; it doesn't change any of it.  It does add a few section
    header comments.
    
    The upstream version doesn't work with Arduino-Makefile; some
    functions are used before they're declared.  It seems that there's
    some magic that the Arduino IDE does that Arduino-Makefile doesn't.
    (IIRC, the IDE wraps everything in one big class.)  This organization
    makes sure that everything is defined (and hence declared) before it's
    used.

--- BLE-Black-Robot-Rover/BLE-Black-Robot-Rover.ino
+++ BLE-Black-Robot-Rover/BLE-Black-Robot-Rover.ino
@@ -29,6 +29,20 @@
 
 #include <Adafruit_MotorShield.h>
 
+//
+// Utilities
+//
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+//
+// Motor
+//
+
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
@@ -36,12 +50,95 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *L_MOTOR = AFMS.getMotor(3);
 Adafruit_DCMotor *R_MOTOR = AFMS.getMotor(4);
 
+// Set your forward, reverse, and turning speeds
+#define ForwardSpeed                255
+#define ReverseSpeed                255
+#define TurningSpeed                100
+
+//
+// Bluetooth
+//
+
 //Name your RC here
 String BROADCAST_NAME = "Adafruit Black Robot Rover";
 
 String BROADCAST_CMD = String("AT+GAPDEVNAME=" + BROADCAST_NAME);
 
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+// function prototypes over in packetparser.cpp
+uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout);
+float parsefloat(uint8_t *buffer);
+void printHex(const uint8_t * data, const uint32_t numBytes);
+
+// the packet buffer
+extern uint8_t packetbuffer[];
+
+char buf[60];
+
+void BLEsetup(){
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  /* Perform a factory reset to make sure everything is in a known state */
+  Serial.println(F("Performing a factory reset: "));
+  if (! ble.factoryReset() ){
+       error(F("Couldn't factory reset"));
+  }
+
+  //Convert the name change command to a char array
+  BROADCAST_CMD.toCharArray(buf, 60);
+
+  //Change the broadcast device name here!
+  if(ble.sendCommandCheckOK(buf)){
+    Serial.println("name changed");
+  }
+  delay(250);
+
+  //reset to take effect
+  if(ble.sendCommandCheckOK("ATZ")){
+    Serial.println("resetting");
+  }
+  delay(250);
+
+  //Confirm name change
+  ble.sendCommandCheckOK("AT+GAPDEVNAME");
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in Controller mode"));
+  Serial.println(F("Then activate/use the sensors, color picker, game controller, etc!"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  Serial.println(F("*****************"));
+
+  // Set Bluefruit to DATA mode
+  Serial.println( F("Switching to DATA mode!") );
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
+  Serial.println(F("*****************"));
+}
+
+//
+// Music
+//
 
 #define toneC    1911
 #define toneC1    1804
@@ -74,68 +171,24 @@ int speaker = A1;
 long vel = 20000;
 boolean hasplayed = false;
 
-// A small helper
-void error(const __FlashStringHelper*err) {
-  Serial.println(err);
-  while (1);
-}
-
-// function prototypes over in packetparser.cpp
-uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout);
-float parsefloat(uint8_t *buffer);
-void printHex(const uint8_t * data, const uint32_t numBytes);
-
-// the packet buffer
-extern uint8_t packetbuffer[];
-
-char buf[60];
-
-// Set your forward, reverse, and turning speeds
-#define ForwardSpeed                255
-#define ReverseSpeed                255
-#define TurningSpeed                100
-
-
-/**************************************************************************/
-/*!
-    @brief  Sets up the HW and the BLE module (this function is called
-            automatically on startup)
-*/
-/**************************************************************************/
-void setup(void)
-{
-  Serial.begin(9600);
-
-  AFMS.begin();  // create with the default frequency 1.6KHz
-
-  // turn on motors
-  L_MOTOR->setSpeed(0);
-  L_MOTOR->run(RELEASE);
-
-  R_MOTOR->setSpeed(0);
-  R_MOTOR->run(RELEASE);
-    
-  Serial.begin(115200);
-  Serial.println(F("Adafruit Bluefruit Robot Controller Example"));
-  Serial.println(F("-----------------------------------------"));
-
-  /* Initialize the module */
-  BLEsetup();
-  
-  pinMode(speaker, OUTPUT);
-}
-
 int melod[] = {tonec, toneG, toneE, toneA, toneB, toneBb, toneA, toneG, tonee, toneg, tonea, tonef, toneg, tonee, tonec, toned, toneB};
 int ritmo[] = {18, 18, 18, 12, 12, 6, 12, 8, 8, 8, 12, 6, 12, 12, 6, 6, 6};
 
-void loop(void)
-{
-  // read new packet data
-  uint8_t len = readPacket(&ble, BLE_READPACKET_TIMEOUT);
-
-  readController();
-    
+void tocar(int tom, long tempo_value) {
+  long tempo_gasto = 0;
+  while (tempo_gasto < tempo_value) {
+    digitalWrite(speaker, HIGH);
+    delayMicroseconds(tom / 2);
+ 
+    digitalWrite(speaker, LOW);
+    delayMicroseconds(tom/2);  
+    tempo_gasto += tom;
+  }
 }
+
+//
+// Controller
+//
 
 bool isMoving = false;
 unsigned long lastPress = 0;
@@ -231,74 +284,45 @@ bool readController(){
 }
 }
 
-void BLEsetup(){
-  Serial.print(F("Initialising the Bluefruit LE module: "));
+//
+// Entry points
+//
 
-  if ( !ble.begin(VERBOSE_MODE) )
-  {
-    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
-  }
-  Serial.println( F("OK!") );
+/**************************************************************************/
+/*!
+    @brief  Sets up the HW and the BLE module (this function is called
+            automatically on startup)
+*/
+/**************************************************************************/
+void setup(void)
+{
+  Serial.begin(9600);
 
-  /* Perform a factory reset to make sure everything is in a known state */
-  Serial.println(F("Performing a factory reset: "));
-  if (! ble.factoryReset() ){
-       error(F("Couldn't factory reset"));
-  }
+  AFMS.begin();  // create with the default frequency 1.6KHz
 
-  //Convert the name change command to a char array
-  BROADCAST_CMD.toCharArray(buf, 60);
+  // turn on motors
+  L_MOTOR->setSpeed(0);
+  L_MOTOR->run(RELEASE);
 
-  //Change the broadcast device name here!
-  if(ble.sendCommandCheckOK(buf)){
-    Serial.println("name changed");
-  }
-  delay(250);
+  R_MOTOR->setSpeed(0);
+  R_MOTOR->run(RELEASE);
+    
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit Robot Controller Example"));
+  Serial.println(F("-----------------------------------------"));
 
-  //reset to take effect
-  if(ble.sendCommandCheckOK("ATZ")){
-    Serial.println("resetting");
-  }
-  delay(250);
-
-  //Confirm name change
-  ble.sendCommandCheckOK("AT+GAPDEVNAME");
-
-  /* Disable command echo from Bluefruit */
-  ble.echo(false);
-
-  Serial.println("Requesting Bluefruit info:");
-  /* Print Bluefruit information */
-  ble.info();
-
-  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in Controller mode"));
-  Serial.println(F("Then activate/use the sensors, color picker, game controller, etc!"));
-  Serial.println();
-
-  ble.verbose(false);  // debug info is a little annoying after this point!
-
-  /* Wait for connection */
-  while (! ble.isConnected()) {
-      delay(500);
-  }
-
-  Serial.println(F("*****************"));
-
-  // Set Bluefruit to DATA mode
-  Serial.println( F("Switching to DATA mode!") );
-  ble.setMode(BLUEFRUIT_MODE_DATA);
-
-  Serial.println(F("*****************"));
+  /* Initialize the module */
+  BLEsetup();
+  
+  pinMode(speaker, OUTPUT);
 }
 
-void tocar(int tom, long tempo_value) {
-  long tempo_gasto = 0;
-  while (tempo_gasto < tempo_value) {
-    digitalWrite(speaker, HIGH);
-    delayMicroseconds(tom / 2);
- 
-    digitalWrite(speaker, LOW);
-    delayMicroseconds(tom/2);  
-    tempo_gasto += tom;
-  }
+void loop(void)
+{
+  // read new packet data
+  uint8_t len = readPacket(&ble, BLE_READPACKET_TIMEOUT);
+
+  readController();
+    
 }
+
