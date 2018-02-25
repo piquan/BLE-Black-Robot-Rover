928bffbe5dd354e498b61b6825e2a62bcb970493
Joel Ray Holveck <joelh@piquan.org>
2018-02-24 17:39:32 -0800

    Formatting fixups
    The upstream code used a variety of formatting conventions.  This
    normalizes the formatting.
    
    This is mostly whitespace changes and comment reflows.  Some strings
    were broken lexically to fit on one line, but they're identical
    strings.  Nothing in this commit should change what the compiler sees.

--- BLE-Black-Robot-Rover/BLE-Black-Robot-Rover.ino
+++ BLE-Black-Robot-Rover/BLE-Black-Robot-Rover.ino
@@ -1,25 +1,24 @@
-/* Erin and Joel's Robot
- *
- * This is code that's based on the Adafruit Adabox 002; see
- * https://learn.adafruit.com/adabox002/ .  I've cleaned it up,
- * changed it to my own whims, and added many new bugs.
- *
- * The Adafruit copyright notice is as follows:
- * 
- *   This is an example for our nRF51822 based Bluefruit LE modules
- *    
- *   Modified to drive a 3-wheeled BLE Robot Rover! by http://james.devi.to
- *   
- *   Pick one up today in the Adafruit shop!
- *   
- *   Adafruit invests time and resources providing this open source code,
- *   please support Adafruit and open-source hardware by purchasing
- *   products from Adafruit!
- *   
- *   MIT license, check LICENSE for more information
- *   All text above, and the splash screen below must be included in
- *   any redistribution
- */
+// Erin and Joel's Robot
+//
+// This is code that's based on the Adafruit Adabox 002; see
+// https://learn.adafruit.com/adabox002/ .  I've cleaned it up,
+// changed it to my own whims, and added many new bugs.
+//
+// The Adafruit copyright notice is as follows:
+//
+//   This is an example for our nRF51822 based Bluefruit LE modules
+//
+//   Modified to drive a 3-wheeled BLE Robot Rover! by http://james.devi.to
+//
+//   Pick one up today in the Adafruit shop!
+//
+//   Adafruit invests time and resources providing this open source code,
+//   please support Adafruit and open-source hardware by purchasing
+//   products from Adafruit!
+//
+//   MIT license, check LICENSE for more information
+//   All text above, and the splash screen below must be included in
+//   any redistribution
 
 #include <Arduino.h>
 #include <Adafruit_BLE.h>
@@ -34,9 +33,11 @@
 //
 
 // A small helper
-void error(const __FlashStringHelper*err) {
-  Serial.println(err);
-  while (1);
+void
+error(const __FlashStringHelper* err)
+{
+    Serial.println(err);
+    while (1);
 }
 
 //
@@ -44,7 +45,7 @@ void error(const __FlashStringHelper*err) {
 //
 
 // Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // And connect 2 DC motors to port M3 & M4 !
 Adafruit_DCMotor *L_MOTOR = AFMS.getMotor(3);
@@ -59,99 +60,103 @@ Adafruit_DCMotor *R_MOTOR = AFMS.getMotor(4);
 // Bluetooth
 //
 
-//Name your RC here
+// Name your RC here
 String BROADCAST_NAME = "Adafruit Black Robot Rover";
 
 String BROADCAST_CMD = String("AT+GAPDEVNAME=" + BROADCAST_NAME);
 
-Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ,
+                             BLUEFRUIT_SPI_RST);
 
 // function prototypes over in packetparser.cpp
-uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout);
-float parsefloat(uint8_t *buffer);
-void printHex(const uint8_t * data, const uint32_t numBytes);
+uint8_t readPacket(Adafruit_BLE* ble, uint16_t timeout);
+float parsefloat(uint8_t* buffer);
+void printHex(const uint8_t* data, const uint32_t numBytes);
 
 // the packet buffer
 extern uint8_t packetbuffer[];
 
 char buf[60];
 
-void BLEsetup(){
-  Serial.print(F("Initialising the Bluefruit LE module: "));
+void
+BLEsetup()
+{
+    Serial.print(F("Initialising the Bluefruit LE module: "));
 
-  if ( !ble.begin(VERBOSE_MODE) )
-  {
-    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
-  }
-  Serial.println( F("OK!") );
+    if (!ble.begin(VERBOSE_MODE)) {
+        error(F("Couldn't find Bluefruit, make sure it's in "
+                "CoMmanD mode & check wiring?"));
+    }
+    Serial.println(F("OK!"));
 
-  /* Perform a factory reset to make sure everything is in a known state */
-  Serial.println(F("Performing a factory reset: "));
-  if (! ble.factoryReset() ){
-       error(F("Couldn't factory reset"));
-  }
+    // Perform a factory reset to make sure everything is in a known state
+    Serial.println(F("Performing a factory reset: "));
+    if (!ble.factoryReset()) {
+        error(F("Couldn't factory reset"));
+    }
+    // Convert the name change command to a char array
+    BROADCAST_CMD.toCharArray(buf, 60);
 
-  //Convert the name change command to a char array
-  BROADCAST_CMD.toCharArray(buf, 60);
+    // Change the broadcast device name here!
+    if (ble.sendCommandCheckOK(buf)) {
+        Serial.println("name changed");
+    }
+    delay(250);
 
-  //Change the broadcast device name here!
-  if(ble.sendCommandCheckOK(buf)){
-    Serial.println("name changed");
-  }
-  delay(250);
+    // reset to take effect
+    if (ble.sendCommandCheckOK("ATZ")) {
+        Serial.println("resetting");
+    }
+    delay(250);
 
-  //reset to take effect
-  if(ble.sendCommandCheckOK("ATZ")){
-    Serial.println("resetting");
-  }
-  delay(250);
+    // Confirm name change
+    ble.sendCommandCheckOK("AT+GAPDEVNAME");
 
-  //Confirm name change
-  ble.sendCommandCheckOK("AT+GAPDEVNAME");
+    // Disable command echo from Bluefruit
+    ble.echo(false);
 
-  /* Disable command echo from Bluefruit */
-  ble.echo(false);
+    Serial.println("Requesting Bluefruit info:");
+    // Print Bluefruit information
+    ble.info();
 
-  Serial.println("Requesting Bluefruit info:");
-  /* Print Bluefruit information */
-  ble.info();
+    Serial.println(F("Please use Adafruit Bluefruit LE app to "
+                     "connect in Controller mode"));
+    Serial.println(F("Then activate/use the sensors, color picker, "
+                     "game controller, etc!"));
+    Serial.println();
 
-  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in Controller mode"));
-  Serial.println(F("Then activate/use the sensors, color picker, game controller, etc!"));
-  Serial.println();
+    ble.verbose(false); // debug info is a little annoying after this point!
 
-  ble.verbose(false);  // debug info is a little annoying after this point!
+    // Wait for connection
+    while (!ble.isConnected()) {
+        delay(500);
+    }
 
-  /* Wait for connection */
-  while (! ble.isConnected()) {
-      delay(500);
-  }
+    Serial.println(F("*****************"));
 
-  Serial.println(F("*****************"));
+    // Set Bluefruit to DATA mode
+    Serial.println(F("Switching to DATA mode!"));
+    ble.setMode(BLUEFRUIT_MODE_DATA);
 
-  // Set Bluefruit to DATA mode
-  Serial.println( F("Switching to DATA mode!") );
-  ble.setMode(BLUEFRUIT_MODE_DATA);
-
-  Serial.println(F("*****************"));
+    Serial.println(F("*****************"));
 }
 
 //
 // Music
 //
 
-#define toneC    1911
-#define toneC1    1804
-#define toneD    1703
-#define toneEb    1607
-#define toneE    1517
-#define toneF    1432
-#define toneF1    1352
-#define toneG    1276
-#define toneAb    1204
-#define toneA    1136
-#define toneBb    1073
-#define toneB    1012
+#define toneC      1911
+#define toneC1     1804
+#define toneD      1703
+#define toneEb     1607
+#define toneE      1517
+#define toneF      1432
+#define toneF1     1352
+#define toneG      1276
+#define toneAb     1204
+#define toneA      1136
+#define toneBb     1073
+#define toneB      1012
 #define tonec       955
 #define tonec1      902
 #define toned       851
@@ -164,26 +169,31 @@ void BLEsetup(){
 #define tonea       568
 #define tonebb      536
 #define toneb       506
- 
-#define tonep       0 
- 
-int speaker = A1; 
+
+#define tonep         0
+
+int speaker = A1;
 long vel = 20000;
 boolean hasplayed = false;
 
-int melod[] = {tonec, toneG, toneE, toneA, toneB, toneBb, toneA, toneG, tonee, toneg, tonea, tonef, toneg, tonee, tonec, toned, toneB};
-int ritmo[] = {18, 18, 18, 12, 12, 6, 12, 8, 8, 8, 12, 6, 12, 12, 6, 6, 6};
+int melod[] = {
+    tonec, toneG, toneE, toneA, toneB, toneBb, toneA, toneG, tonee,
+    toneg, tonea, tonef, toneg, tonee, tonec, toned, toneB
+};
+int ritmo[] = { 18, 18, 18, 12, 12, 6, 12, 8, 8, 8, 12, 6, 12, 12, 6, 6, 6 };
 
-void tocar(int tom, long tempo_value) {
-  long tempo_gasto = 0;
-  while (tempo_gasto < tempo_value) {
-    digitalWrite(speaker, HIGH);
-    delayMicroseconds(tom / 2);
- 
-    digitalWrite(speaker, LOW);
-    delayMicroseconds(tom/2);  
-    tempo_gasto += tom;
-  }
+void
+tocar(int tom, long tempo_value)
+{
+    long tempo_gasto = 0;
+    while (tempo_gasto < tempo_value) {
+        digitalWrite(speaker, HIGH);
+        delayMicroseconds(tom / 2);
+
+        digitalWrite(speaker, LOW);
+        delayMicroseconds(tom / 2);
+        tempo_gasto += tom;
+    }
 }
 
 //
@@ -193,95 +203,99 @@ void tocar(int tom, long tempo_value) {
 bool isMoving = false;
 unsigned long lastPress = 0;
 
-bool readController(){
-  uint8_t maxspeed;
+bool
+readController()
+{
+    uint8_t maxspeed;
 
- // Buttons
-  if (packetbuffer[1] == 'B') {
+    // Buttons
+    if (packetbuffer[1] == 'B') {
 
-    uint8_t buttnum = packetbuffer[2] - '0';
-    boolean pressed = packetbuffer[3] - '0';
+        uint8_t buttnum = packetbuffer[2] - '0';
+        boolean pressed = packetbuffer[3] - '0';
 
-    // Serial.println(buttnum);
+        // Serial.println(buttnum);
 
-    if (pressed) {
-      if(buttnum == 1){
-        if (hasplayed == true){ return;}
-          for (int i=0; i<17; i++) {
-            int tom = melod[i];
-            int tempo = ritmo[i];
- 
-            long tvalue = tempo * vel;
- 
-            tocar(tom, tvalue);
- 
-            delayMicroseconds(1000);
-            }      //delay(1000);
- 
-            hasplayed = true;
-      }
-      
-      if(buttnum == 2){
-        
-      }
+        if (pressed) {
+            if (buttnum == 1) {
+                if (hasplayed == true) {
+                    return;
+                }
+                for (int i = 0; i < 17; i++) {
+                    int tom = melod[i];
+                    int tempo = ritmo[i];
 
-      if(buttnum == 3){
-        
-      }
+                    long tvalue = tempo * vel;
 
-      if(buttnum == 4){
-        
-      }
+                    tocar(tom, tvalue);
 
-      if(buttnum == 5){
-        isMoving = true;
-        L_MOTOR->run(FORWARD);
-        R_MOTOR->run(FORWARD);
-        maxspeed = ForwardSpeed;
-      }
-      
-      if(buttnum == 6){
-        isMoving = true;
-        L_MOTOR->run(BACKWARD);
-        R_MOTOR->run(BACKWARD);
-        maxspeed = ReverseSpeed;        
-      }
-      
-      if(buttnum == 7){
-        isMoving = true;
-        L_MOTOR->run(RELEASE);
-        R_MOTOR->run(FORWARD);
-        maxspeed = TurningSpeed;
-      }
-      
-      if(buttnum == 8){
-        isMoving = true;
-        L_MOTOR->run(FORWARD);
-        R_MOTOR->run(RELEASE);
-        maxspeed = TurningSpeed;        
-      }
+                    delayMicroseconds(1000);
+                }		// delay(1000);
 
-      lastPress = millis();
+                hasplayed = true;
+            }
 
-      // speed up the motors
-      for (int speed=0; speed < maxspeed; speed+=5) {
-        L_MOTOR->setSpeed(speed);
-        R_MOTOR->setSpeed(speed);
-        delay(5); // 250ms total to speed up
-      }
-  } else {
-      isMoving = false;
-      // slow down the motors
-      for (int speed = maxspeed; speed >= 0; speed-=5) {
-        L_MOTOR->setSpeed(speed);
-        R_MOTOR->setSpeed(speed);
-        delay(5); // 50ms total to slow down
-      }
-      L_MOTOR->run(RELEASE);
-      R_MOTOR->run(RELEASE);
-      hasplayed = false;
+            if (buttnum == 2) {
+
+            }
+
+            if (buttnum == 3) {
+
+            }
+
+            if (buttnum == 4) {
+
+            }
+
+            if (buttnum == 5) {
+                isMoving = true;
+                L_MOTOR->run(FORWARD);
+                R_MOTOR->run(FORWARD);
+                maxspeed = ForwardSpeed;
+            }
+
+            if (buttnum == 6) {
+                isMoving = true;
+                L_MOTOR->run(BACKWARD);
+                R_MOTOR->run(BACKWARD);
+                maxspeed = ReverseSpeed;
+            }
+
+            if (buttnum == 7) {
+                isMoving = true;
+                L_MOTOR->run(RELEASE);
+                R_MOTOR->run(FORWARD);
+                maxspeed = TurningSpeed;
+            }
+
+            if (buttnum == 8) {
+                isMoving = true;
+                L_MOTOR->run(FORWARD);
+                R_MOTOR->run(RELEASE);
+                maxspeed = TurningSpeed;
+            }
+
+            lastPress = millis();
+
+            // speed up the motors
+            for (int speed = 0; speed < maxspeed; speed += 5) {
+                L_MOTOR->setSpeed(speed);
+                R_MOTOR->setSpeed(speed);
+                delay(5);	// 250ms total to speed up
+            }
+        } else {
+            isMoving = false;
+            // slow down the motors
+            for (int speed = maxspeed; speed >= 0; speed -= 5) {
+                L_MOTOR->setSpeed(speed);
+                R_MOTOR->setSpeed(speed);
+                delay(5);	// 50ms total to slow down
+            }
+            L_MOTOR->run(RELEASE);
+            R_MOTOR->run(RELEASE);
+            hasplayed = false;
+        }
     }
-}
 }
 
 //
@@ -290,39 +304,39 @@ bool readController(){
 
 /**************************************************************************/
 /*!
-    @brief  Sets up the HW and the BLE module (this function is called
-            automatically on startup)
-*/
+ * @brief Sets up the HW and the BLE module (this function is called
+ *        automatically on startup)
+ */
 /**************************************************************************/
-void setup(void)
+void
+setup(void)
 {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  AFMS.begin();  // create with the default frequency 1.6KHz
+    AFMS.begin();		// create with the default frequency 1.6KHz
 
-  // turn on motors
-  L_MOTOR->setSpeed(0);
-  L_MOTOR->run(RELEASE);
+    // turn on motors
+    L_MOTOR->setSpeed(0);
+    L_MOTOR->run(RELEASE);
 
-  R_MOTOR->setSpeed(0);
-  R_MOTOR->run(RELEASE);
-    
-  Serial.begin(115200);
-  Serial.println(F("Adafruit Bluefruit Robot Controller Example"));
-  Serial.println(F("-----------------------------------------"));
+    R_MOTOR->setSpeed(0);
+    R_MOTOR->run(RELEASE);
 
-  /* Initialize the module */
-  BLEsetup();
-  
-  pinMode(speaker, OUTPUT);
+    Serial.begin(115200);
+    Serial.println(F("Adafruit Bluefruit Robot Controller Example"));
+    Serial.println(F("-----------------------------------------"));
+
+    // Initialize the module
+    BLEsetup();
+
+    pinMode(speaker, OUTPUT);
 }
 
-void loop(void)
+void
+loop(void)
 {
-  // read new packet data
-  uint8_t len = readPacket(&ble, BLE_READPACKET_TIMEOUT);
+    // read new packet data
+    uint8_t len = readPacket(&ble, BLE_READPACKET_TIMEOUT);
 
-  readController();
-    
+    readController();
 }
-
